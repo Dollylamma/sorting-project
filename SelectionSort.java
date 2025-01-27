bd5e3b9c798444e45ea8e3564953afd1f2b50601
dexterchn <dexterchn@outlook.com>
2025-01-27 09:25:34 -0800

    Update SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,6 +1,18 @@
 public class SelectionSort implements Sorter {
 
     public void sort(int[] input) {
-        System.out.println("Selection Sort!!!");
+        int pmin, temp; 
+        for(int i=0; i<input.length; i++){
+            pmin=i;
+            for(int j=i+1; j<input.length; j++){
+            if(input[pmin]>input[j]){
+                pmin=j;
+            }
+            }
+            temp= input[pmin];
+            input[pmin]=input[i];
+            input[i]=temp;
+        }
+        return input;
     }
 }
