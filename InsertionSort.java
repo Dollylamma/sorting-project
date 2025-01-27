e6fcff9b11417d1bc4e04a776adb28727a821230
dexterchn <dexterchn@outlook.com>
2025-01-27 15:08:41 -0800

    added insertion sort

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,6 +1,15 @@
 public class InsertionSort implements Sorter {
 
     public void sort(int[] input) {
-        System.out.println("Insertion Sort!!!");
+        for(int i=1; i<input.length; i++){
+            for(int j=i; j>0; j--){
+                int temp;
+                if(input[j]<input[j-1]){
+                    temp=input[j];
+                    input[j]=input[j-1];
+                    input[j-1]=temp;
+                }
+            }
+        }
     }
 }
