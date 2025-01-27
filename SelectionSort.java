789e3d84b5136b4b788556e1ee2fa77600ade13a
dexterchn <dexterchn@outlook.com>
2025-01-27 15:08:55 -0800

    added selection sort

--- SelectionSort.java
+++ SelectionSort.java
@@ -5,14 +5,13 @@ public class SelectionSort implements Sorter {
         for(int i=0; i<input.length; i++){
             pmin=i;
             for(int j=i+1; j<input.length; j++){
-            if(input[pmin]>input[j]){
-                pmin=j;
-            }
+                if(input[pmin]>input[j]){
+                    pmin=j;
+                }
             }
             temp= input[pmin];
             input[pmin]=input[i];
             input[i]=temp;
         }
-        return input;
     }
 }
