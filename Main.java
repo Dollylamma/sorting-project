344db7d8af236012b37e195e711d65e0c6ecbe13
dexterchn <dexterchn@outlook.com>
2025-01-27 15:08:49 -0800

    added random test cases

--- Main.java
+++ Main.java
@@ -11,10 +11,13 @@ public class Main {
         //     of InsertionSort and Selection sort continue to work.
         //  4. Increase the loop count for TestSuite to get better performance results.
 
-        int[] testInput = { 1, 2, 3, 1 };
+        int[] testInput = new int[10];
+        for(int i=0; i<10; i++){
+            testInput[i]=(int)(Math.random()*100);
+        }
 
         // Increase the loop count to get better results once it works.
         // TestSuite.run(testInput, 10000);
-        TestSuite.run(testInput, 1);
+        TestSuite.run(testInput, 10);
     }
 }
